2e8e859aef5268e007ace8838cc781de2045ee58
sanledi buli <sanledi.buli@gmail.com>
2014-08-27 19:16:06 +0700

    Initializer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BantuinTa::Application.config.secret_key_base = '108a844098f1444836627c2e98fed8a5e0a1423236a7d540710b63d271afa5ac9ea6d855db32197a5f4ad298a803eb1cc4b358fba2a3ff3d74e1b699bcf6a5dc'

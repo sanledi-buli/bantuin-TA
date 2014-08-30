ea4afa347edcd10e2d27089bf921b802c6a0267e
sanledi buli <sanledi.buli@gmail.com>
2014-08-30 17:49:59 +0700

    random secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BantuinTa::Application.config.secret_key_base = '108a844098f1444836627c2e98fed8a5e0a1423236a7d540710b63d271afa5ac9ea6d855db32197a5f4ad298a803eb1cc4b358fba2a3ff3d74e1b699bcf6a5dc'
+BantuinTa::Application.config.secret_key_base = SecureRandom.hex(64)

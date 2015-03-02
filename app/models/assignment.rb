12f39bc08d5156823e1527ee0ec1b745d9d03dc6
sanledi buli <sanledi.buli@gmail.com>
2015-03-03 00:13:53 +0700

    validation and associations on role and assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,6 @@
 class Assignment < ActiveRecord::Base
+  validates :description, presence: true, uniqueness: { case_sensitive: false }
+
+  has_many :roles, through: :role_assignments
+  has_many :role_assignments
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,11 @@
 class Role < ActiveRecord::Base
-	has_many :users
+  
+  #validations
+
+  validates :name, presence: true
+  
+  #associations
+  has_many :users
+  has_many :assignments, through: :role_assignments
+  has_many :role_assignments
 end
